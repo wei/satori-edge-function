766b96d1dce82b7e302ffa91a0535edc6360b203
Wei He <git@weispot.com>
2022-11-12 22:23:02 -0500

    Add Satori custom font route

---
+++ pages/api/satori-custom-font.tsx
@@ -0,0 +1,49 @@
+import satori from 'satori'
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+const font = fetch(new URL('../../assets/TYPEWR__.TTF', import.meta.url)).then(
+  (res) => res.arrayBuffer()
+)
+
+export default async function handler() {
+  const fontData = await font
+
+  const svg = await satori(
+    (
+      <div
+        style={{
+          backgroundColor: 'white',
+          height: '100%',
+          width: '100%',
+          fontSize: 100,
+          fontFamily: 'Typewriter',
+          paddingTop: '100px',
+          paddingLeft: '50px',
+        }}
+      >
+        Hello world!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: 'Typewriter',
+          data: fontData,
+          style: 'normal',
+        },
+      ],
+    }
+  )
+
+  return new Response(svg, {
+    status: 200,
+    headers: {
+      'Content-Type': 'image/svg+xml',
+    }
+  })
+}
